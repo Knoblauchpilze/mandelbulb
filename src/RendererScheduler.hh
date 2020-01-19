83948a3d21f0b124d8bbadfd8f9702e67daab770
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-01-19 11:41:42 +0100

    Added base implementation for the scheduler of the raytracing

--- src/RendererScheduler.hh
+++ src/RendererScheduler.hh
@@ -1,8 +1,68 @@
 #ifndef    RENDERER_SCHEDULER_HH
 # define   RENDERER_SCHEDULER_HH
 
+# include <mutex>
+# include <memory>
+# include <core_utils/CoreObject.hh>
+# include <maths_utils/Box.hh>
+# include <core_utils/ThreadPool.hh>
+
 namespace mandelbulb {
 
+  class RendererScheduler: utils::CoreObject {
+    public:
+
+      RendererScheduler();
+
+      ~RendererScheduler();
+
+      void
+      start();
+
+      void
+      stop();
+
+    private:
+
+      static
+      unsigned
+      getWorkerThreadCount() noexcept;
+
+      void
+      build();
+
+      void
+      scheduleRendering();
+
+      void
+      handleTilesRenderer(const std::vector<utils::AsynchronousJobShPtr>& tiles);
+
+    private:
+
+      /**
+       * @brief - Describe the possible state for the computation.
+       */
+      enum class Rendering {
+        Converged,
+        Accumulating
+      };
+
+      std::mutex m_propsLocker;
+
+      utils::ThreadPoolShPtr m_scheduler;
+
+      Rendering m_computation;
+
+      unsigned m_taskProgress;
+
+      unsigned m_taskTotal;
+
+    public:
+
+      utils::Signal<utils::Boxf> onTileRendered;
+  };
+
+  using RendererSchedulerShPtr = std::shared_ptr<RendererScheduler>;
 }
 
 # include "RendererScheduler.hxx"
--- src/RendererScheduler.hxx
+++ src/RendererScheduler.hxx
@@ -5,6 +5,18 @@
 
 namespace mandelbulb {
 
+  inline
+  RendererScheduler::~RendererScheduler() {
+    // Stops the rendering.
+    stop();
+  }
+
+  inline
+  unsigned
+  RendererScheduler::getWorkerThreadCount() noexcept {
+    return 3u;
+  }
+
 }
 
 #endif    /* RENDERER_SCHEDULER_HXX */
